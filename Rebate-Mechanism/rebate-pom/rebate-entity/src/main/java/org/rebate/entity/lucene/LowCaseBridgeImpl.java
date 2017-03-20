2ff7d746b19932bf14a73359d06e8b74835cde0d
shijun <shijun@cienet.com.cn>
2017-03-20 14:12:12 +0800

    删除 @Override

--- Rebate-Mechanism/rebate-pom/rebate-entity/src/main/java/org/rebate/entity/lucene/LowCaseBridgeImpl.java
+++ Rebate-Mechanism/rebate-pom/rebate-entity/src/main/java/org/rebate/entity/lucene/LowCaseBridgeImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.search.bridge.LuceneOptions;
 
 public class LowCaseBridgeImpl implements FieldBridge {
 
-  @Override
   public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
     if (value instanceof String) {
       String field = ((String) value).toLowerCase();
