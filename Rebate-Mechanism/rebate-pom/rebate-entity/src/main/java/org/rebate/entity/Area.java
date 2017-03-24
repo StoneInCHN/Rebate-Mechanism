601c3abba9dd05eb8dbe8cd07b9fc43212dbe1e0
huyong <82370288@qq.com>
2017-03-24 15:33:52 +0800

    add isCity and isHotCity in Area

--- Rebate-Mechanism/rebate-pom/rebate-entity/src/main/java/org/rebate/entity/Area.java
+++ Rebate-Mechanism/rebate-pom/rebate-entity/src/main/java/org/rebate/entity/Area.java
@@ -28,169 +28,200 @@ import org.rebate.entity.base.OrderEntity;
 @SequenceGenerator(name = "sequenceGenerator", sequenceName = "rm_area_sequence")
 public class Area extends OrderEntity {
 
-  private static final long serialVersionUID = -2158109459123036967L;
+	private static final long serialVersionUID = -2158109459123036967L;
 
-  /** 树路径分隔符 */
-  private static final String TREE_PATH_SEPARATOR = ",";
+	/** 树路径分隔符 */
+	private static final String TREE_PATH_SEPARATOR = ",";
 
-  /** 名称 */
-  private String name;
+	/** 名称 */
+	private String name;
 
-  /** 全称 */
-  private String fullName;
+	/** 全称 */
+	private String fullName;
 
-  /** 树路径 */
-  private String treePath;
+	/** 树路径 */
+	private String treePath;
 
-  /** 上级地区 */
-  private Area parent;
+	/** 上级地区 */
+	private Area parent;
 
-  /** 下级地区 */
-  private Set<Area> children = new HashSet<Area>();
+	/** 下级地区 */
+	private Set<Area> children = new HashSet<Area>();
 
+	/**
+	 * 是否为市级（包含直辖市）
+	 */
+	private Boolean isCity;
 
-  /**
-   * 获取名称
-   * 
-   * @return 名称
-   */
-  @NotEmpty
-  @Length(max = 100)
-  @Column(nullable = false, length = 100)
-  public String getName() {
-    return name;
-  }
+	/**
+	 * 是否为热门城市
+	 */
+	private Boolean isHotCity;
 
-  /**
-   * 设置名称
-   * 
-   * @param name 名称
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
+	/**
+	 * 获取名称
+	 * 
+	 * @return 名称
+	 */
+	@NotEmpty
+	@Length(max = 100)
+	@Column(nullable = false, length = 100)
+	public String getName() {
+		return name;
+	}
 
-  /**
-   * 获取全称
-   * 
-   * @return 全称
-   */
-  @Column(nullable = false, length = 500)
-  public String getFullName() {
-    return fullName;
-  }
+	/**
+	 * 设置名称
+	 * 
+	 * @param name
+	 *            名称
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  /**
-   * 设置全称
-   * 
-   * @param fullName 全称
-   */
-  public void setFullName(String fullName) {
-    this.fullName = fullName;
-  }
+	/**
+	 * 获取全称
+	 * 
+	 * @return 全称
+	 */
+	@Column(nullable = false, length = 500)
+	public String getFullName() {
+		return fullName;
+	}
 
-  /**
-   * 获取树路径
-   * 
-   * @return 树路径
-   */
-  @Column(nullable = false, updatable = false)
-  public String getTreePath() {
-    return treePath;
-  }
+	/**
+	 * 设置全称
+	 * 
+	 * @param fullName
+	 *            全称
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
-  /**
-   * 设置树路径
-   * 
-   * @param treePath 树路径
-   */
-  public void setTreePath(String treePath) {
-    this.treePath = treePath;
-  }
+	/**
+	 * 获取树路径
+	 * 
+	 * @return 树路径
+	 */
+	@Column(nullable = false, updatable = false)
+	public String getTreePath() {
+		return treePath;
+	}
 
-  /**
-   * 获取上级地区
-   * 
-   * @return 上级地区
-   */
-  @ManyToOne(fetch = FetchType.LAZY)
-  public Area getParent() {
-    return parent;
-  }
+	/**
+	 * 设置树路径
+	 * 
+	 * @param treePath
+	 *            树路径
+	 */
+	public void setTreePath(String treePath) {
+		this.treePath = treePath;
+	}
 
-  /**
-   * 设置上级地区
-   * 
-   * @param parent 上级地区
-   */
-  public void setParent(Area parent) {
-    this.parent = parent;
-  }
+	/**
+	 * 获取上级地区
+	 * 
+	 * @return 上级地区
+	 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	public Area getParent() {
+		return parent;
+	}
 
-  /**
-   * 获取下级地区
-   * 
-   * @return 下级地区
-   */
-  @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
-  @OrderBy("order asc")
-  public Set<Area> getChildren() {
-    return children;
-  }
+	/**
+	 * 设置上级地区
+	 * 
+	 * @param parent
+	 *            上级地区
+	 */
+	public void setParent(Area parent) {
+		this.parent = parent;
+	}
 
-  /**
-   * 设置下级地区
-   * 
-   * @param children 下级地区
-   */
-  public void setChildren(Set<Area> children) {
-    this.children = children;
-  }
+	/**
+	 * 获取下级地区
+	 * 
+	 * @return 下级地区
+	 */
+	@OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	@OrderBy("order asc")
+	public Set<Area> getChildren() {
+		return children;
+	}
 
-  /*
-   * @OneToMany(mappedBy = "area", fetch = FetchType.LAZY) public Set<Distributor> getVendors() {
-   * return vendors; }
-   * 
-   * public void setVendors(Set<Distributor> vendors) { this.vendors = vendors; }
-   */
+	/**
+	 * 设置下级地区
+	 * 
+	 * @param children
+	 *            下级地区
+	 */
+	public void setChildren(Set<Area> children) {
+		this.children = children;
+	}
 
-  /**
-   * 持久化前处理
-   */
-  @PrePersist
-  public void prePersist() {
-    Area parent = getParent();
-    if (parent != null) {
-      setFullName(parent.getFullName() + getName());
-      setTreePath(parent.getTreePath() + parent.getId() + TREE_PATH_SEPARATOR);
-    } else {
-      setFullName(getName());
-      setTreePath(TREE_PATH_SEPARATOR);
-    }
-  }
+	/*
+	 * @OneToMany(mappedBy = "area", fetch = FetchType.LAZY) public
+	 * Set<Distributor> getVendors() { return vendors; }
+	 * 
+	 * public void setVendors(Set<Distributor> vendors) { this.vendors =
+	 * vendors; }
+	 */
 
-  /**
-   * 更新前处理
-   */
-  @PreUpdate
-  public void preUpdate() {
-    Area parent = getParent();
-    if (parent != null) {
-      setFullName(parent.getFullName() + getName());
-    } else {
-      setFullName(getName());
-    }
-  }
+	/**
+	 * 持久化前处理
+	 */
+	@PrePersist
+	public void prePersist() {
+		Area parent = getParent();
+		if (parent != null) {
+			setFullName(parent.getFullName() + getName());
+			setTreePath(parent.getTreePath() + parent.getId()
+					+ TREE_PATH_SEPARATOR);
+		} else {
+			setFullName(getName());
+			setTreePath(TREE_PATH_SEPARATOR);
+		}
+	}
 
+	/**
+	 * 更新前处理
+	 */
+	@PreUpdate
+	public void preUpdate() {
+		Area parent = getParent();
+		if (parent != null) {
+			setFullName(parent.getFullName() + getName());
+		} else {
+			setFullName(getName());
+		}
+	}
 
-  /**
-   * 重写toString方法
-   * 
-   * @return 全称
-   */
-  @Override
-  public String toString() {
-    return getFullName();
-  }
+	/**
+	 * 重写toString方法
+	 * 
+	 * @return 全称
+	 */
+	@Override
+	public String toString() {
+		return getFullName();
+	}
+
+	public Boolean getIsCity() {
+		return isCity;
+	}
+
+	public void setIsCity(Boolean isCity) {
+		this.isCity = isCity;
+	}
+
+	public Boolean getIsHotCity() {
+		return isHotCity;
+	}
+
+	public void setIsHotCity(Boolean isHotCity) {
+		this.isHotCity = isHotCity;
+	}
 
 }
