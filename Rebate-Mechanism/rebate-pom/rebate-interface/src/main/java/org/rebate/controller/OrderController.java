051ffc89c9a98620b75ccda89d45eb2cab78e287
huyong <82370288@qq.com>
2017-03-28 22:15:50 +0800

    merge code

--- Rebate-Mechanism/rebate-pom/rebate-interface/src/main/java/org/rebate/controller/OrderController.java
+++ Rebate-Mechanism/rebate-pom/rebate-interface/src/main/java/org/rebate/controller/OrderController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.rebate.beans.CommonAttributes;
+import org.rebate.beans.Message;
 import org.rebate.controller.base.MobileBaseController;
 import org.rebate.entity.EndUser;
 import org.rebate.entity.Order;
@@ -18,6 +19,7 @@ import org.rebate.json.base.ResponseOne;
 import org.rebate.service.EndUserService;
 import org.rebate.service.OrderService;
 import org.rebate.utils.FieldFilterUtils;
+import org.rebate.utils.TokenGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,32 +54,32 @@ public class OrderController extends MobileBaseController {
 
 		Long userId = request.getUserId();
 		String token = request.getToken();
-		// Long areaId = request.getAreaId();
 		EndUser endUser = endUserService.find(userId);
 
 		// 验证登录token
-		// String userToken = endUserService.getEndUserToken(userId);
-		// if (!TokenGenerator.isValiableToken(token, userToken)) {
-		// response.setCode(CommonAttributes.FAIL_TOKEN_TIMEOUT);
-		// response.setDesc(Message.error("csh.user.token.timeout")
-		// .getContent());
-		// return response;
-		// }
+		String userToken = endUserService.getEndUserToken(userId);
+		if (!TokenGenerator.isValiableToken(token, userToken)) {
+			response.setCode(CommonAttributes.FAIL_TOKEN_TIMEOUT);
+			response.setDesc(Message.error("rebate.user.token.timeout")
+					.getContent());
+			return response;
+		}
 
 		List<Filter> filters = new ArrayList<Filter>();
 		Filter endUserFilter = new Filter("endUser", Operator.eq, endUser);
 		filters.add(endUserFilter);
 
 		List<Order> list = orderService.findList(null, filters, null);
-		String[] propertys = { "id", "seller.name", "userScore", "amount",
-				"evaluate" };
+		String[] propertys = { "id", "sn", "seller.name", "userScore",
+				"amount", "createDate", "evaluate", "remark",
+				"evaluate.content", "evaluate.sellerReply" };
 		List<Map<String, Object>> result = FieldFilterUtils
 				.filterCollectionMap(propertys, list);
 
 		response.setMsg(result);
-		// String newtoken = TokenGenerator.generateToken(request.getToken());
-		// endUserService.createEndUserToken(newtoken, userId);
-		// response.setToken(newtoken);
+		String newtoken = TokenGenerator.generateToken(request.getToken());
+		endUserService.createEndUserToken(newtoken, userId);
+		response.setToken(newtoken);
 		response.setCode(CommonAttributes.SUCCESS);
 		return response;
 	}
@@ -108,7 +110,7 @@ public class OrderController extends MobileBaseController {
 
 		Order order = orderService.find(orderId);
 		String[] propertys = { "id", "seller.name", "userScore", "amount",
-				"evaluate" };
+				"evaluate.content", "evaluate.sellerReply" };
 		Map<String, Object> result = FieldFilterUtils.filterEntityMap(
 				propertys, order);
 
