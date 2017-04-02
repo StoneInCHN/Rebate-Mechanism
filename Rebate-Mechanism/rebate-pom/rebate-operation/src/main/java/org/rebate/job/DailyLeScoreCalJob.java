b3582dc8d58f0c559479c282c24559741de5a900
Andrea <464709367@qq.com>
2017-04-02 14:19:58 +0800

    add job

---
+++ Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/job/DailyLeScoreCalJob.java
@@ -0,0 +1,32 @@
+package org.rebate.job;
+
+import javax.annotation.Resource;
+
+import org.rebate.service.EndUserService;
+import org.rebate.utils.LogUtil;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy(false)
+public class DailyLeScoreCalJob {
+
+
+  @Resource(name = "endUserServiceImpl")
+  private EndUserService endUserService;
+
+
+  // @Scheduled(cron="0/10 * *  * * ? ") //每10秒执行一次
+  @Scheduled(cron = "59 59 23 * * ?")
+  // 每天23点59分59秒执行
+  public void dailyLeScoreCalculate() {
+
+    LogUtil.debug(DailyLeScoreCalJob.class, "dailyLeScoreCalculate",
+        "daily user leScore calculate start !");
+
+    //
+    LogUtil.debug(DailyLeScoreCalJob.class, "dailyLeScoreCalculate",
+        "daily user leScore calculate end!");
+  }
+}
