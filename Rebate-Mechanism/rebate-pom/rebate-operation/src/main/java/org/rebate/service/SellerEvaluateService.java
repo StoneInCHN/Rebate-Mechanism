80626d63346ddc9305d2fa8132d0e71f75e8092c
Andrea <464709367@qq.com>
2017-05-15 17:05:56 +0800

    add field status for seller evaluate

--- Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/service/SellerEvaluateService.java
+++ Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/service/SellerEvaluateService.java
@@ -1,8 +1,16 @@
-package org.rebate.service; 
+package org.rebate.service;
 
 import org.rebate.entity.SellerEvaluate;
 import org.rebate.framework.service.BaseService;
 
-public interface SellerEvaluateService extends BaseService<SellerEvaluate,Long>{
+public interface SellerEvaluateService extends BaseService<SellerEvaluate, Long> {
 
+
+  // /**
+  // * 更改商家评价状态
+  // *
+  // * @param orderId
+  // * @return
+  // */
+  // SellerEvaluate changeEvaluateStatus(Long Id);
 }
