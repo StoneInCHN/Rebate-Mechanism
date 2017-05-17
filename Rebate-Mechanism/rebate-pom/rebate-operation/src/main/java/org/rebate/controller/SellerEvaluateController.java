10bf17e687058f1b653677a3e0353a9c45b79853
Andrea <464709367@qq.com>
2017-05-17 16:35:20 +0800

    fix bug:seller evaluate display

--- Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/controller/SellerEvaluateController.java
+++ Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/controller/SellerEvaluateController.java
@@ -1,14 +1,12 @@
 package org.rebate.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.rebate.beans.Message;
 import org.rebate.controller.base.BaseController;
-import org.rebate.entity.SellerEvaluate;
 import org.rebate.entity.commonenum.CommonEnum.CommonStatus;
 import org.rebate.framework.filter.Filter;
 import org.rebate.framework.filter.Filter.Operator;
@@ -25,30 +23,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller("sellerEvaluateController")
 public class SellerEvaluateController extends BaseController {
 
-  @Resource(name="sellerEvaluateServiceImpl")
+  @Resource(name = "sellerEvaluateServiceImpl")
   private SellerEvaluateService sellerEvaluateService;
-  
+
   /**
    * 列表
    */
   @RequestMapping(value = "/list", method = RequestMethod.GET)
-  public String list(Pageable pageable,SellerEvaluateRequest request, ModelMap model) {
+  public String list(Pageable pageable, SellerEvaluateRequest request, ModelMap model) {
 
     List<Filter> filters = new ArrayList<Filter>();
     if (request.getUserName() != null) {
-      Filter userNameFilter = new Filter("endUser&nickName", Operator.like, "%"+request.getUserName()+"%");
+      Filter userNameFilter =
+          new Filter("endUser&nickName", Operator.like, "%" + request.getUserName() + "%");
       filters.add(userNameFilter);
     }
     if (request.getSellerName() != null) {
-      Filter sellerFilter = new Filter("seller&name", Operator.like, "%"+request.getSellerName()+"%");
+      Filter sellerFilter =
+          new Filter("seller&name", Operator.like, "%" + request.getSellerName() + "%");
       filters.add(sellerFilter);
     }
     if (request.getScore() != null) {
-      Filter scoreFilter = new Filter("score", Operator.eq,request.getScore());
+      Filter scoreFilter = new Filter("score", Operator.eq, request.getScore());
       filters.add(scoreFilter);
     }
     if (request.getBeginDate() != null) {
-      Filter dateGeFilter = new Filter("createDate", Operator.ge, request.getBeginDate() );
+      Filter dateGeFilter = new Filter("createDate", Operator.ge, request.getBeginDate());
       filters.add(dateGeFilter);
     }
     if (request.getEndDate() != null) {
@@ -64,43 +64,31 @@ public class SellerEvaluateController extends BaseController {
     model.addAttribute("score", request.getScore());
     return "/sellerEvaluate/list";
   }
-  
+
   /**
    * 启用
    */
   @RequestMapping(value = "/active", method = RequestMethod.POST)
   public @ResponseBody Message active(Long[] ids) {
-    if(ids==null || ids.length <1){
+    if (ids == null || ids.length < 1) {
       return ERROR_MESSAGE;
     }
-    List<SellerEvaluate> lists = new ArrayList<SellerEvaluate>();
-    List<SellerEvaluate> sellerEvaluates = sellerEvaluateService.findList(ids);
-    for (SellerEvaluate sellerEvaluate : sellerEvaluates) {
-      sellerEvaluate.setStatus(CommonStatus.ACITVE);
-      lists.add(sellerEvaluate);
-    }
-    sellerEvaluateService.update(lists);
+    sellerEvaluateService.changeEvaluateStatus(ids, CommonStatus.ACITVE);
     return SUCCESS_MESSAGE;
   }
-  
+
   /**
    * 禁用
    */
   @RequestMapping(value = "/inActive", method = RequestMethod.POST)
   public @ResponseBody Message inActive(Long[] ids) {
-    if(ids==null || ids.length <1){
+    if (ids == null || ids.length < 1) {
       return ERROR_MESSAGE;
     }
-    List<SellerEvaluate> lists = new ArrayList<SellerEvaluate>();
-    List<SellerEvaluate> sellerEvaluates = sellerEvaluateService.findList(ids);
-    for (SellerEvaluate sellerEvaluate : sellerEvaluates) {
-      sellerEvaluate.setStatus(CommonStatus.INACTIVE);
-      lists.add(sellerEvaluate);
-    }
-    sellerEvaluateService.update(lists);
+    sellerEvaluateService.changeEvaluateStatus(ids, CommonStatus.INACTIVE);
     return SUCCESS_MESSAGE;
   }
-  
+
   /**
    * 详情
    */
@@ -109,5 +97,5 @@ public class SellerEvaluateController extends BaseController {
     model.addAttribute("sellerEvaluate", sellerEvaluateService.find(id));
     return "/sellerEvaluate/details";
   }
-  
+
 }
