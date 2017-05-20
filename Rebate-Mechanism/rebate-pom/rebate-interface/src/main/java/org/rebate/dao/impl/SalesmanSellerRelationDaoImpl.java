9be8075c55c04d22ff2ae1c2fdfb511c64256117
Andrea <464709367@qq.com>
2017-05-21 00:23:17 +0800

    salesman commission

--- Rebate-Mechanism/rebate-pom/rebate-interface/src/main/java/org/rebate/dao/impl/SalesmanSellerRelationDaoImpl.java
+++ Rebate-Mechanism/rebate-pom/rebate-interface/src/main/java/org/rebate/dao/impl/SalesmanSellerRelationDaoImpl.java
@@ -1,11 +1,31 @@
-package org.rebate.dao.impl; 
+package org.rebate.dao.impl;
 
-import org.springframework.stereotype.Repository; 
+import javax.persistence.FlushModeType;
+import javax.persistence.NoResultException;
 
-import org.rebate.entity.SalesmanSellerRelation;
-import org.rebate.framework.dao.impl.BaseDaoImpl;
 import org.rebate.dao.SalesmanSellerRelationDao;
+import org.rebate.entity.SalesmanSellerRelation;
+import org.rebate.entity.Seller;
+import org.rebate.framework.dao.impl.BaseDaoImpl;
+import org.springframework.stereotype.Repository;
+
 @Repository("salesmanSellerRelationDaoImpl")
-public class SalesmanSellerRelationDaoImpl extends  BaseDaoImpl<SalesmanSellerRelation,Long> implements SalesmanSellerRelationDao {
+public class SalesmanSellerRelationDaoImpl extends BaseDaoImpl<SalesmanSellerRelation, Long>
+    implements SalesmanSellerRelationDao {
+
+  @Override
+  public SalesmanSellerRelation getRelationBySeller(Seller seller) {
+    if (seller == null) {
+      return null;
+    }
+    try {
+      String jpql =
+          "select relation from SalesmanSellerRelation relation where relation.seller = :seller";
+      return entityManager.createQuery(jpql, SalesmanSellerRelation.class)
+          .setFlushMode(FlushModeType.COMMIT).setParameter("seller", seller).getSingleResult();
+    } catch (NoResultException e) {
+      return null;
+    }
+  }
 
 }
