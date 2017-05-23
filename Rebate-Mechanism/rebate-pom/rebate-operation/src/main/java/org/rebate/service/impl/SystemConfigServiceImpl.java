62aba2b724009d0ee83183b34a0444768b9f1702
tankboo <tankboo@163.com>
2017-05-23 18:21:03 +0800

    基础数据配置

--- Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/service/impl/SystemConfigServiceImpl.java
+++ Rebate-Mechanism/rebate-pom/rebate-operation/src/main/java/org/rebate/service/impl/SystemConfigServiceImpl.java
@@ -8,6 +8,8 @@ import org.rebate.entity.commonenum.CommonEnum.SystemConfigKey;
 import org.rebate.framework.service.impl.BaseServiceImpl;
 import org.rebate.service.SystemConfigService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("systemConfigServiceImpl")
 public class SystemConfigServiceImpl extends BaseServiceImpl<SystemConfig, Long> implements
@@ -25,4 +27,24 @@ public class SystemConfigServiceImpl extends BaseServiceImpl<SystemConfig, Long>
   public SystemConfig getConfigByKey(SystemConfigKey key) {
     return systemConfigDao.getConfigByKey(key);
   }
+
+  @Override
+  @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+  public SystemConfig update(SystemConfig entity) {
+    if(SystemConfigKey.TRANSACTION_FEE_PERCENTAGE.equals(entity.getConfigKey()) &&  entity.getIsEnabled()){
+      SystemConfig config = getConfigByKey(SystemConfigKey.TRANSACTION_FEE_PERTIME);
+      if(config!=null&&config.getIsEnabled()){
+        config.setIsEnabled(false);
+        super.update(config);
+      }
+    }else if (SystemConfigKey.TRANSACTION_FEE_PERTIME.equals(entity.getConfigKey()) && entity.getIsEnabled()) {
+      SystemConfig config = getConfigByKey(SystemConfigKey.TRANSACTION_FEE_PERCENTAGE);
+      if(config!=null&&config.getIsEnabled()){
+        config.setIsEnabled(false);
+        super.update(config);
+      }
+    }
+    return super.update(entity);
+  }
+  
 }
